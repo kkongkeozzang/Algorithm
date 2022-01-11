ca8d06d4e5b612f8fa59e286e36e325e356dffbd
KKONGKEOZZANG <suzie1642@gmail.com>
2022-01-11 16:31:19 +0900

    BOJ2753

---
+++ Baekjoon/src/BOJ2753.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class BOJ2753 {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        sc.close();
+
+        if(n%4 == 0 && (n%100 != 0 || n%400 == 0)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+
+    }
+}
