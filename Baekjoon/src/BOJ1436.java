0866b9ec07a671e266609a3762b372aa556c1a65
KKONGKEOZZANG <suzie1642@gmail.com>
2022-03-18 14:11:17 +0900

    BOJ1436

---
+++ Baekjoon/src/BOJ1436.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class BOJ1436 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(bf.readLine());
+        int num = 666;
+        int count = 1;
+        while(count!=n) {
+            num++;
+            if(String.valueOf(num).contains("666")) {
+                count++;
+            }
+        }
+        System.out.println(num);
+    }
+}
