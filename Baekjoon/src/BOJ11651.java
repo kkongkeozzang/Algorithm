8a2ed4ca07aa8085696a6949eba22945a4dec049
KKONGKEOZZANG <suzie1642@gmail.com>
2022-03-26 16:54:17 +0900

    BOJ11651

--- Baekjoon/src/BOJ11651.java
+++ Baekjoon/src/BOJ11651.java
@@ -1,26 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
 
-public class BOJ11650 {
+public class BOJ11651 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         List<int[]> arr = new ArrayList<>();
-        // List에 좌표 넣기
         while(n --> 0) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
             int[] tmp = new int[2];
+            StringTokenizer st = new StringTokenizer(br.readLine());
             tmp[0] = Integer.parseInt(st.nextToken());
             tmp[1] = Integer.parseInt(st.nextToken());
             arr.add(tmp);
         }
         arr.sort((o1, o2) -> {
-            if(o1[0] == o2[0]) {
-                return o1[1] - o2[1];
-            } else {
+            if(o1[1] == o2[1]) {
                 return o1[0] - o2[0];
+            } else {
+                return o1[1] - o2[1];
             }
         });
         StringBuilder sb = new StringBuilder();
