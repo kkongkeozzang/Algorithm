f040f0c7f3f24fae5e0560ed19c50a3544c72b72
KKONGKEOZZANG <suzie1642@gmail.com>
2022-04-17 21:01:41 +0900

    BOJ1927

---
+++ Baekjoon/src/BOJ1927.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class BOJ1927 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int n = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        while(n-->0) {
+            String s = br.readLine();
+            if(s.equals("0")) {
+                if(pq.isEmpty()) {
+                    sb.append(0).append("\n");
+                } else {
+                    sb.append(pq.poll()).append("\n");
+                }
+            } else {
+                pq.add(Integer.parseInt(s));
+            }
+
+        }
+        System.out.println(sb);
+    }
+
+}
