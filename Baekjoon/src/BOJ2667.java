ac33b0eb9a0c9bb2dc1b9502ba0104efa7daf7b0
KKONGKEOZZANG <suzie1642@gmail.com>
2022-04-25 23:56:33 +0900

    BOJ2667

---
+++ Baekjoon/src/BOJ2667.java
@@ -0,0 +1,84 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class BOJ2667 {
+    static int[][] apart;
+    static boolean[][] checked;
+    static int[] count;
+    static int countNum = 0;
+    static int n;
+
+
+    static int[] dx = {0,0,-1,1};
+    static int[] dy = {-1,1,0,0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        apart = new int[n][n];
+        checked = new boolean[n][n];
+        count = new int[n*n];
+
+        for(int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for(int j = 0; j < n; j++) {
+                apart[i][j] = s.charAt(j) - '0';
+            }
+        }
+
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(apart[i][j] != 0 && !checked[i][j]) {
+                    countNum++;
+                    bfs(i,j);
+                }
+            }
+        }
+
+        Arrays.sort(count);
+        sb.append(countNum).append("\n");
+
+        for(int i : count) {
+            if(i != 0) {
+                sb.append(i).append("\n");
+            }
+        }
+
+        System.out.println(sb);
+
+    }
+
+    static void bfs(int x, int y) {
+        Queue<Integer[]> q = new LinkedList<>();
+        checked[x][y] = true;
+        q.offer(new Integer[] {x,y});
+        count[countNum]++;
+
+        while(!q.isEmpty()) {
+            Integer[] target = q.poll();
+            int tX = target[0];
+            int tY = target[1];
+
+            for(int i = 0; i < dx.length; i++) {
+                tX += dx[i];
+                tY += dy[i];
+
+                if(tX >= 0 && tY >= 0 && tX < n && tY < n) {
+                    if((apart[tX][tY] != 0) && !checked[tX][tY]) {
+                        q.offer(new Integer[] {tX, tY});
+                        checked[tX][tY] = true;
+                        count[countNum]++;
+                    }
+                }
+            }
+
+        }
+    }
+}
